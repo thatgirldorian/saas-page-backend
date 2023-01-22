3f2e08e602756449aa68d7f2c6e524916b46a05b
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-01-22 20:06:19 +0400

    Add endpoint for login path

--- saas-be/app.js
+++ saas-be/app.js
@@ -4,4 +4,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("Hello from the other sideee!"));
 
+app.post("/login", (req, res) => {});
+
 app.listen(3001, () => console.log("Server ready"));
