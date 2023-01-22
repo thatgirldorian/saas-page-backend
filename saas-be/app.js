ee98fefa148d01dc50a51dbef8eb01409ac3309d
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-01-22 20:10:01 +0400

    Extracted email and ps

--- saas-be/app.js
+++ saas-be/app.js
@@ -4,6 +4,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("Hello from the other sideee!"));
 
-app.post("/login", (req, res) => {});
+app.post("/login", (req, res) => {
+  console.log("Login data was received");
+
+  const email = req.body.email;
+  const password = req.body.password;
+});
 
 app.listen(3001, () => console.log("Server ready"));
